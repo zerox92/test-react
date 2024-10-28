596b4dd067cf0ac7f5a20966f21a33fd8db98c39
Zeeshan Anjum <zeerox92@gmail.com>
2024-10-28 13:16:41 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Maryam, MZ - ta da cha yee?????
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zeeshan ;-))))))
         </a>
       </header>
     </div>
