14a4694fcd636174dae3a623bd421431b52c8b18
Zeeshan Anjum <zeerox92@gmail.com>
2024-10-28 13:21:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        MZ, Ta da cha ye yarooon??? Say ka loudly !!
+    
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zeeshan ;-))))
         </a>
       </header>
     </div>
